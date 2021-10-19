3f838a8135168b0382476eced3a6389a47e89c18
sec77 <m.kimpel@secure77.de>
2021-10-19 19:27:03 +0200

    kali setup

--- install.sh
+++ install.sh
@@ -9,10 +9,6 @@ cd nerd-fonts
 ./install.sh
 cd ..
 
-git clone https://github.com/jwilm/alacritty
-cd alacritty
-cargo build --release && sudo cp target/release/alacritty /usr/local/bin
-cd ..
 
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps && mkdir -p build && cd build && meson ..
@@ -22,19 +18,29 @@ cd ../..
 
 pip3 install pywal
 
+
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/compton
 mkdir -p ~/.config/rofi
-mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/qterminal.org
+mkdir -p ~/.wallpaper
 cp .config/i3/config ~/.config/i3/config
-cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 cp .config/compton/compton.conf ~/.config/compton/compton.conf
 cp .config/rofi/config ~/.config/rofi/config
 cp .fehbg ~/.fehbg
 cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
 
+cp .config/.xsessionrc ~/.xsessionrc
+cp .config/qterminal.org/qterminal.ini ~/.config/qterminal.org/qterminal.ini 
+
+
 echo "Done! Grab some wallpaper and run pywal -i filename to set your color scheme. To have the wallpaper set on every boot edit ~.fehbg"
 echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+cp .config/.zshrc ~/.zshrc
+sudo systemctl enable xrdp
+sudo systemctl start xrdp
