57b1bb2a614f2b84f1114891a7209d6632db1db2
sec77 <m.kimpel@secure77.de>
2023-08-21 17:32:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd ~
 sudo apt update && sudo apt upgrade -y
 
 #sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
-sudo apt-get install -y arandr arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick xrdp seclists feroxbuster golang gobuster rlwrap bloodhound.py neo4j bloodhound crowbar ntpdate xxd
+sudo apt install -y arandr arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick xrdp seclists feroxbuster golang gobuster rlwrap bloodhound.py neo4j bloodhound crowbar ntpdate xxd
 # sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 # sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
 
@@ -27,7 +27,7 @@ go install github.com/sensepost/gowitness@latest
 
 
 
-git clone https://www.github.com/Airblader/i3 i3-gaps
+#git clone https://www.github.com/Airblader/i3 i3-gaps
 # cd i3-gaps && mkdir -p build && cd build && meson ..
 # ninja
 # sudo ninja install
@@ -81,5 +81,5 @@ source .zshrc
 
 
 # only if we need rdp activ
-#sudo systemctl enable xrdp
-#sudo systemctl start xrdp
+sudo systemctl enable xrdp
+sudo systemctl start xrdp
